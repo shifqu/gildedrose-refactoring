a017dd7f3fa9969342451a7574030a4d534b73bb
Sonny V <sonny@softllama.net>
2022-04-30 22:24:15 +0200

    test: use pytest and avoid *-imports
    Also allow testtext_fixture.py to write a json file. A file can then be
    written from the original code, and used to compare new results.

--- tests/fixtures/testtext_fixture.py
+++ tests/fixtures/testtext_fixture.py
@@ -6,7 +6,25 @@ See `https://texttest.org/` for more information on texttests.
 """
 from __future__ import print_function
 
-from gildedrose_refactoring.gilded_rose import *
+import json
+from collections import defaultdict
+from pathlib import Path
+
+from gildedrose_refactoring.gilded_rose import GildedRose, Item
+from tests.test_gilded_rose import destructure
+
+
+def _write(items):
+    path = Path(__file__).parent / "gilded_items.json"
+    if path.exists():
+        input_ = input(f"The file {path} already exists, ok to overwrite? (y/N)")
+        if input_.lower() not in ["yes", "y"]:
+            print("User aborted the write, file exists.")
+            return
+
+    with path.open("w") as fp:
+        json.dump(items, fp)
+
 
 if __name__ == "__main__":
     print("OMGHAI!")
@@ -22,15 +40,20 @@ if __name__ == "__main__":
         Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
     ]
 
-    days = 2
+    days = 17
     import sys
 
     if len(sys.argv) > 1:
         days = int(sys.argv[1]) + 1
+
+    result = defaultdict(list)
     for day in range(days):
         print("-------- day %s --------" % day)
         print("name, sellIn, quality")
         for item in items:
             print(item)
         print("")
+        result[day] = [destructure(item) for item in items]
         GildedRose(items).update_quality()
+
+    _write(result)
--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,16 +1,102 @@
-# -*- coding: utf-8 -*-
-import unittest
+"""Module containing all GildedRose related fixtures, tests and test-utils."""
+import json
+from pathlib import Path
+
+import pytest
 
 from gildedrose_refactoring.gilded_rose import GildedRose, Item
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+@pytest.fixture(name="gilded_rose")
+def fixture_gilded_rose(items: list[Item]) -> GildedRose:
+    return GildedRose(items)
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture(name="items")
+def fixture_items() -> list[Item]:
+    return [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
+
+
+@pytest.fixture(name="items_after_update")
+def fixture_items_after_update() -> list[Item]:
+    return [
+        Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
+        Item(name="Aged Brie", sell_in=1, quality=1),
+        Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=14, quality=21),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=9, quality=50),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=4, quality=50),
+        Item(name="Conjured Mana Cake", sell_in=2, quality=5),  # <-- :O
+    ]
+
+
+@pytest.fixture(name="items_json")
+def fixture_items_json() -> dict[str, list[dict]]:
+    path = Path(__file__).parent / "fixtures" / "gilded_items.json"
+    assert path.is_file(), f"Expected {path} to be an existing file."
+    with path.open() as fp:
+        items_json: dict[str, list[dict]] = json.load(fp)
+
+    return items_json
+
+
+def destructure(item: Item):
+    fields = ["name", "sell_in", "quality"]
+    return {field: getattr(item, field) for field in fields}
+
+
+def assert_items_eq(items: list[Item], other_items: list[Item]) -> None:
+    """Assert that items and other_items are equal.
+
+    For an item to be equal to another, their `name`, `sell_in` and `quality` should be equal.
+    """
+    for item1, item2 in zip(items, other_items, strict=True):
+        _assert_item_eq(item1, item2)
+
+
+def _assert_item_eq(item1: Item, item2: Item) -> None:
+    assert item1.name == item2.name
+    assert item1.sell_in == item2.sell_in
+    assert item1.quality == item2.quality
+
+
+def test_update_quality(gilded_rose: GildedRose, items_after_update: list[Item]) -> None:
+    gilded_rose.update_quality()
+    assert_items_eq(gilded_rose.items, items_after_update)
+
+
+def test_update_multiple_days(items: list[Item], items_json: dict[str, list[dict]]) -> None:
+    result: dict[str, list[dict]] = {}
+    for day in range(17):
+        day_str = str(day)
+        result[day_str] = [destructure(item) for item in items]
+        GildedRose(items).update_quality()
+
+    result_len = len(result)
+    items_json_len = len(items_json)
+    assert result_len <= items_json_len, (
+        f"The length of the result ({result_len}) should be less or equal to the length of the "
+        f"fixture ({items_json_len}). hint: try to lower the day-range in this test."
+    )
+
+    for key in result.keys():
+        i1 = [Item(**item) for item in result[key]]
+        i2 = [Item(**item) for item in items_json[key]]
+        assert_items_eq(i1, i2)
+
+
+def test_item_repr():
+    item = Item("Test", 1, 1)
+    assert str(item) == "Test, 1, 1"
