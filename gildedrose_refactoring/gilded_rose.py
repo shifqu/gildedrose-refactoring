5621bb0248f2881681d389c5b6539d9cb264b1e9
Sonny V <sonny@softllama.net>
2022-05-01 13:52:57 +0200

    refactor(gilded_rose): simplify logic and increase readability
    This commit also removes unused boilerplate files

--- gildedrose_refactoring/gilded_rose.py
+++ gildedrose_refactoring/gilded_rose.py
@@ -1,4 +1,83 @@
 """Module containing the core objects used by the Gilded Rose."""
+from dataclasses import dataclass
+
+
+@dataclass()
+class GildedItem:
+    """Represent an Item with extra functionality to update the sell_in and quality."""
+
+    name: str
+    sell_in: int
+    quality: int
+
+    def is_backstagepass(self) -> bool:
+        """Check if the item is a backstagepass."""
+        if self.name.startswith("Backstage passes"):
+            return True
+        return False
+
+    def is_better_with_age(self) -> bool:
+        """Check if the item's quality should increase over time."""
+        if self.name == "Aged Brie":
+            return True
+        return False
+
+    def is_expired(self) -> bool:
+        """Check if the item's sell_in is negative."""
+        if self.sell_in < 0:
+            return True
+        return False
+
+    def is_legendary(self) -> bool:
+        """Check if the item is a legendary item."""
+        if self.name == "Sulfuras, Hand of Ragnaros":
+            return True
+        return False
+
+    def update(self) -> None:
+        """Update the sell_in and the quality of the item."""
+        if self.is_legendary():
+            return  # Do not update legendary items
+
+        self._update_sell_in()
+
+        if self.is_backstagepass():
+            self._update_backstagepass()
+            return
+
+        quality_modifier = 1
+        if self.is_expired():
+            quality_modifier = 2
+
+        if self.is_better_with_age():
+            new_quality = self.quality + quality_modifier
+        else:
+            new_quality = self.quality - quality_modifier
+        self._update_quality(new_quality)
+
+    def _update_backstagepass(self) -> None:
+        if self.is_expired():
+            self.quality = 0
+            return
+        if self.sell_in >= 10:
+            quality_modifier = 1
+        elif self.sell_in >= 5:
+            quality_modifier = 2
+        else:
+            quality_modifier = 3
+        new_quality = self.quality + quality_modifier
+        self._update_quality(new_quality)
+
+    def _update_quality(self, quality: int) -> None:
+        if quality < 0:
+            self.quality = 0
+        elif quality > 50:
+            self.quality = 50
+        else:
+            self.quality = quality
+
+    def _update_sell_in(self) -> None:
+        self.sell_in -= 1
 
 
 class GildedRose:
@@ -14,37 +93,11 @@ class GildedRose:
         ----------
         See `GildedRoseRequirements.txt` for more information on this method.
         """
-        for item in self.items:
-            if (
-                item.name != "Aged Brie"
-                and item.name != "Backstage passes to a TAFKAL80ETC concert"
-            ):
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+        for i, item in enumerate(self.items):
+            gilded_item = GildedItem(item.name, item.sell_in, item.quality)
+            gilded_item.update()
+            item = Item(gilded_item.name, gilded_item.sell_in, gilded_item.quality)
+            self.items[i] = item
 
 
 class Item:
